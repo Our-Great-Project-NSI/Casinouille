24f031a718ae36ac4bf5ac94716d17c561e03ac4
LinkStrox <127395918+Likstrox@users.noreply.github.com>
2024-04-02 14:34:00 +0200

    Update MainRP.py

--- MainRP.py
+++ MainRP.py
@@ -13,7 +13,6 @@ WIDTH, HEIGHT = infoObject.current_w, infoObject.current_h
 screen = pygame.display.set_mode(size)
 font = pygame.font.SysFont(None, 64)
 GAMEBG = pygame.image.load('grille.png')
-
 bg_width = GAMEBG.get_width()
 bg_height = GAMEBG.get_height()
 scale_factor = WIDTH / bg_width
@@ -21,13 +20,13 @@ scaled_width = int(bg_width * scale_factor)
 scaled_height = int(bg_height * scale_factor)
 scaled_bg = pygame.transform.scale(GAMEBG, (scaled_width, scaled_height))
 
-game_background = pygame.transform.scale(GAMEBG, (WIDTH, HEIGHT))
+
 while True:
     for event in pygame.event.get():
         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
             pygame.quit()
         elif event.type == MOUSEBUTTONDOWN:
-            screen.blit(scaled_bg, (0, 0))
+            screen.blit(scaled_bg, (WIDTH / 2 - scaled_width / 2, HEIGHT / 2 - scaled_height / 2))
 
     pygame.display.flip()
 
